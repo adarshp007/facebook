55d122fcd91e55e39f000c2293fd5af4e46dde54
adarshp007 <adarshmp123@gmail.com>
2021-05-21 01:30:19 +0530

    host web

--- Facebook/settings.py
+++ Facebook/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-ah1pfry+31%&ec6m_ft^xvc#s^uv^1x))wy7kl)2hxo!o1&dsv
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -41,6 +41,7 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
